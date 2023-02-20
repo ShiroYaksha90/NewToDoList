35a9f41ecab55825409af3d16461d66b12afd1ba
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-02-21 00:40:41 +0300

    Create array of objects for to do's tasks Dynamically display each task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const toDos = [{
+  description : 'Fix car',
+  completed: false,
+  index: 1,
+},{
+description : 'Finish milestone 1',
+completed: false,
+index: 2,
+},
+{ description : 'Take out the trash',
+completed: false,
+index: 3,
+}]
+
+const list = document.querySelector('.todo');
+window.addEventListener('DOMContentLoaded', () => {
+  const listString = toDos.map((task) => `<li class="task">
+<div class="item">
+  <input type="checkbox">
+  <p>${task.description}</p>
+</div>
+<i class="fa-solid fa-ellipsis-vertical"></i> </li>`).join('');
+list.innerHTML = listString;
+})
