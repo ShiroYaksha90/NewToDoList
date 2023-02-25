0cba0ef8e99c533e3e58740c2e0d692f19eea861
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-02-25 18:24:31 +0300

    Fix linters issues

--- src/modules/resetIndex.js
+++ src/modules/resetIndex.js
@@ -1,13 +1,13 @@
 const resetIndex = () => {
-    const taskList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
-    const arr = [];
-  
-    taskList.forEach((obj) => {
-      const newObj = { ...obj, index: (arr.length + 1).toString() };
-      arr.push(newObj);
-    });
-    localStorage.setItem('tasks', JSON.stringify(arr));
-    window.location.reload();
-  };
+  const taskList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+  const arr = [];
 
-  export default resetIndex
+  taskList.forEach((obj) => {
+    const newObj = { ...obj, index: (arr.length + 1).toString() };
+    arr.push(newObj);
+  });
+  localStorage.setItem('tasks', JSON.stringify(arr));
+  window.location.reload();
+};
+
+export default resetIndex;
