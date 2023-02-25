cbc8476e8c77d5cbd96e9387c8bf17fa0064492c
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-02-25 18:17:08 +0300

    Add updateStatus.js Add resetIndex.js Update index.js

--- src/modules/add.js
+++ src/modules/add.js
@@ -1,93 +1,128 @@
-import { editLocalStorage, removeFromLocalStorage, resetIndex } from './storage.js';
+// import { editLocalStorage, removeFromLocalStorage } from './storage.js';
+// import resetIndex from './resetIndex'
+// import clearCompletedTask from './clear.js';
+// import updateStatus from './updateStatus.js';
+// const listContainer = document.querySelector('[data-list]');
+// const newListForm = document.querySelector('[data-new-list-form]');
+// const newListInput = document.querySelector('[data-new-list-input]');
+// const clearButton = document.querySelector('clearbtn')
+// const lists = JSON.parse(localStorage.getItem('tasks')) || [];
+// const clearElement = (element) => {
+//   while (element.firstChild) {
+//     element.removeChild(element.firstChild);
+//   }
+// };
 
-const listContainer = document.querySelector('[data-list]');
-const newListForm = document.querySelector('[data-new-list-form]');
-const newListInput = document.querySelector('[data-new-list-input]');
+// const renderTasks = () => {
+//   lists.forEach((list) => {
+//     const listElement = document.createElement('li');
+//     listElement.id = list.index;
+//     listElement.classList.add('task');
+//     listElement.innerHTML = `<div class="item">
+//           <input class="checkbox" type="checkbox">
+//           <p>${list.desc}</p>
+//           </div>
+//           <i class="fa-solid fa-ellipsis-vertical"></i>
+//           <i class="fa-solid fa-trash-clock fa-trash hide"></i>`;
+//     listContainer.appendChild(listElement);
+//     const optionButton = listElement.lastElementChild.previousElementSibling;
+//     const deleteButton = listElement.lastElementChild;
+//     optionButton.addEventListener('click', (e) => {
+//       optionButton.classList.add('hide');
+//       deleteButton.classList.remove('hide');
+//       const edit = e.target.previousElementSibling.lastElementChild;
+//       const editInput = document.createElement('input');
+//       editInput.type = 'text';
+//       editInput.className = 'edit-input';
+//       editInput.required = 'true';
+//       editInput.placeholder = edit.textContent;
+//       const editIndex = e.target.parentElement.id;
+//       editInput.focus();
+//       const nameDiv = e.target.previousElementSibling;
+//       edit.style.display = 'none';
+//       nameDiv.appendChild(editInput);
+//       editInput.addEventListener('keypress', (e) => {
+//         if (e.key === 'Enter') {
+//           edit.textContent = editInput.value;
+//           nameDiv.appendChild(edit);
+//           nameDiv.removeChild(editInput);
+//           optionButton.classList.remove('hide');
+//           deleteButton.classList.add('hide');
+//           edit.style.display = '';
+//         }
+//         editLocalStorage(editInput.value, editIndex);
+//       });
+//     });
+//     deleteButton.addEventListener('click', (e) => {
+//       const task = e.target.parentElement;
+//       const index = task.id;
+//       removeFromLocalStorage(index);
+//       resetIndex();
+//       listContainer.removeChild(task);
+// // localStorage.clear()
+//       window.location.reload();
+//     });
+//     // const checkBox = listElement.querySelector('.checkbox')
+//     // checkBox.addEventListener('change', (e) => {
+//     //   console.log(checkBox)
+//     //   if ( checkBox.checked === true) {
+//     //     const taskDesc = e.target.nextElementSibling
+//     //     taskDesc.style.textDecoration = 'line-through'
+//     //     // const task = listElement.querySelector('task')
+//     //     const index = listElement.id
+//     //     console.log(index)
+//     //     updateStatus(checkBox.checked,index)
+//     //     // tasksList.filter((task) => {
+//     //     //   if(task.index == index) {
+//     //     //     task.status = true
+//     //     //   }
+//     //     //   return false
+//     //     // })
+//     //     // localStorage.setItem('tasks', JSON.stringify(tasksList))
+//     //   } else {
+//     //     const taskDesc = e.target.nextElementSibling
+//     //     taskDesc.style.textDecoration = 'none'
+//     //     const index = listElement.id
+//     //     updateStatus(checkBox.checked, index)
+//     //     // const tasksList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+//     //     // tasksList.filter((task) => {
+//     //     //   if(task.index == index) {
+//     //     //     task.status = false
+//     //     //   }
+//     //     //   return false
+//     //     // })
+//     //     // localStorage.setItem('tasks', JSON.stringify(tasksList))
+//     //   }
+//     //   console.log(localStorage)
+//     // })
+//   });
+// };
 
-const lists = JSON.parse(localStorage.getItem('tasks')) || [];
-const clearElement = (element) => {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-};
+// const render = () => {
+//   clearElement(listContainer);
+//   renderTasks();
+// };
 
-const renderTasks = () => {
-  lists.forEach((list) => {
-    const listElement = document.createElement('li');
-    listElement.id = list.index;
-    listElement.classList.add('task');
-    listElement.innerHTML = `<div class="item">
-          <input type="checkbox">
-          <p>${list.desc}</p>
-          </div>
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-          <i class="fa-solid fa-trash-clock fa-trash hide"></i>`;
-    listContainer.appendChild(listElement);
-    const optionButton = listElement.lastElementChild.previousElementSibling;
-    const deleteButton = listElement.lastElementChild;
-    optionButton.addEventListener('click', (e) => {
-      optionButton.classList.add('hide');
-      deleteButton.classList.remove('hide');
-      const edit = e.target.previousElementSibling.lastElementChild;
-      const editInput = document.createElement('input');
-      editInput.type = 'text';
-      editInput.className = 'edit-input';
-      editInput.required = 'true';
-      editInput.placeholder = edit.textContent;
-      const editIndex = e.target.parentElement.id;
-      editInput.focus();
-      const nameDiv = e.target.previousElementSibling;
-      edit.style.display = 'none';
-      nameDiv.appendChild(editInput);
-      editInput.addEventListener('keypress', (e) => {
-        if (e.key === 'Enter') {
-          edit.textContent = editInput.value;
-          nameDiv.appendChild(edit);
-          nameDiv.removeChild(editInput);
-          optionButton.classList.remove('hide');
-          deleteButton.classList.add('hide');
-          edit.style.display = '';
-        }
-        editLocalStorage(editInput.value, editIndex);
-      });
-    });
-    deleteButton.addEventListener('click', (e) => {
-      const task = e.target.parentElement;
-      const index = task.id;
-      removeFromLocalStorage(index);
-      resetIndex();
-      listContainer.removeChild(task);
+// const save = () => {
+//   localStorage.setItem('tasks', JSON.stringify(lists));
+// };
 
-      window.location.reload();
-    });
-  });
-};
+// const saveAndRender = () => {
+//   save();
+//   render();
+// };
 
-const render = () => {
-  clearElement(listContainer);
-  renderTasks();
-};
-
-const save = () => {
-  localStorage.setItem('tasks', JSON.stringify(lists));
-};
-
-const saveAndRender = () => {
-  save();
-  render();
-};
-
-newListForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const taskObj = {
-    desc: newListInput.value,
-    completed: false,
-    index: (lists.length + 1).toString(),
-  };
-  if (taskObj.desc === null || taskObj.desc === '') return;
-  lists.push(taskObj);
-  newListInput.value = null;
-  saveAndRender();
-});
-
-export default render();
+// newListForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const taskObj = {
+//     desc: newListInput.value,
+//     completed: false,
+//     index: (lists.length + 1).toString(),
+//   };
+//   if (taskObj.desc === null || taskObj.desc === '') return;
+//   lists.push(taskObj);
+//   newListInput.value = null;
+//   saveAndRender();
+// });
+// // clearButton.addEventListener('click', clearCompletedTask())
+// export default render();
---
+++ src/modules/resetIndex.js
@@ -0,0 +1,13 @@
+const resetIndex = () => {
+    const taskList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+    const arr = [];
+  
+    taskList.forEach((obj) => {
+      const newObj = { ...obj, index: (arr.length + 1).toString() };
+      arr.push(newObj);
+    });
+    localStorage.setItem('tasks', JSON.stringify(arr));
+    window.location.reload();
+  };
+
+  export default resetIndex
